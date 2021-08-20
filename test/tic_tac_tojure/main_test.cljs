d5f8b73ddaa6e9eead1aa29292c12fe26292076d
Matthew Ciraula <ciraulam@tutanota.com>
2021-08-19 20:45:58 -0400

    Implemented tests for check-col

--- test/tic_tac_tojure/main_test.cljs
+++ test/tic_tac_tojure/main_test.cljs
@@ -1,7 +1,7 @@
 (ns tic-tac-tojure.main-test
   (:require
    [cljs.test :refer-macros [deftest is testing]]
-   [tic-tac-tojure.main :refer [check-row]]))
+   [tic-tac-tojure.main :refer [check-row check-col]]))
 
 (deftest check-row-test
   (testing "check-row"
@@ -26,3 +26,27 @@
       (testing "2 2s in a row returns false"
         (is (= false
                (check-row b 2)))))))
+
+(deftest check-col-test
+  (testing "check-col"
+    (let [b [[1 0 0]
+             [1 0 1]
+             [1 0 0]]]
+      (testing "1 mark in col returns false"
+        (is (= false
+               (check-col b 2))))
+      (testing "3 1s in a row returns 1"
+        (is (= 1
+               (check-col b 0))))
+      (testing "3 0s in a col returns false"
+        (is (= false
+               (check-col b 1)))))
+    (let [b [[2 0 2]
+             [2 0 0]
+             [2 0 2]]]
+      (testing "3 2s in a col returns 2"
+        (is (= 2
+               (check-col b 0))))
+      (testing "2 2s in a col returns false"
+        (is (= false
+               (check-col b 2)))))))
